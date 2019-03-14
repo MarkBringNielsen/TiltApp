9a73e96424b9d5c969c5d003e86cc19a9761088b
Unknown <markbringnielsen@gmail.com>
2019-03-14 10:46:28 +0100

    Init for app

---
+++ TiltApp/TiltApp/TiltApp/TiltAppViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace TiltApp
+{
+    public class TiltAppViewModel
+    {
+        public String XYZ { get; set; }
+
+        public TiltAppViewModel()
+        {
+            StopCommand = new Command(Stop);
+            StartCommand = new Command(Start);
+        }
+
+        public Command StartCommand { get; }
+        public Command StopCommand { get; }
+
+        void Stop()
+        {
+            OrientationSensor.Stop();
+            OrientationSensor.ReadingChanged -= Orientation_ReadingChanged;
+        }
+
+        void Start()
+        {
+            OrientationSensor.ReadingChanged += Orientation_ReadingChanged;
+            OrientationSensor.Start(SensorSpeed.UI);
+        }
+
+        void Orientation_ReadingChanged(Object sender, OrientationSensorChangedEventArgs e)
+        {
+            XYZ = e.Reading + "";
+        }
+
+
+    }
+}
